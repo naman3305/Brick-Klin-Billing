8eb3b3942955c76c2fbd789d64cef7de1ea53c2d
naman3305 <59595534+naman3305@users.noreply.github.com>
2020-11-20 22:55:38 +0530

    Update conn.java

--- src/brick/klin/conn.java
+++ src/brick/klin/conn.java
@@ -1,14 +1,19 @@
 import java.sql.*;
 
-public class conn {
+public class conn 
+{
      Connection c;
     Statement s;
-    public conn(){  
-        try{  
+    public conn()
+    {  
+        try
+        {  
             Class.forName("com.mysql.jdbc.Driver");  
             c =DriverManager.getConnection("jdbc:mysql://localhost:3306/project1","root",""); 
             s =c.createStatement();  
-        }catch(Exception e){ 
+        }
+         catch(Exception e)
+         { 
             System.out.println(e);
         }  
     }  
