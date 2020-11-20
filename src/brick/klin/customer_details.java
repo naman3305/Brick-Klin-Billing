99eb3741f736db704f9ca6e35b5fad6b567a7b88
naman3305 <59595534+naman3305@users.noreply.github.com>
2020-11-20 22:57:13 +0530

    Update customer_details.java

--- src/brick/klin/customer_details.java
+++ src/brick/klin/customer_details.java
@@ -3,19 +3,22 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.sql.*;
 
-public class customer_details extends JFrame implements ActionListener{
+public class customer_details extends JFrame implements ActionListener
+{
  
     JTable t1;
     JButton b1;
     String x[] = {"Name","Cust_Id","Address","Email","Phone"};
     String y[][] = new String[20][5];
     int i=0, j=0;
-    customer_details(){
+    customer_details()
+    {
         super("Customer Details");
         setSize(1200,650);
         setLocation(200,200);
         
-        try{
+        try
+        {
             conn c1  = new conn();
             String s1 = "select * from customer";
             ResultSet rs  = c1.s.executeQuery(s1);
@@ -30,7 +33,9 @@ public class customer_details extends JFrame implements ActionListener{
             }
             t1 = new JTable(y,x);
             
-        }catch(Exception e){
+        }
+        catch(Exception e)
+        {
             e.printStackTrace();
         }
         
@@ -41,7 +46,8 @@ public class customer_details extends JFrame implements ActionListener{
         add(sp);
         b1.addActionListener(this);
     }
-    public void actionPerformed(ActionEvent ae){
+    public void actionPerformed(ActionEvent ae)
+    {
         try{
             t1.print();
         }catch(Exception e){}
